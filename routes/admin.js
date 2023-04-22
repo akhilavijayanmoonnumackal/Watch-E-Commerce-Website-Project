e82a5187dc5b0b523157889b3abca87d424ba9f5
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-04-23 02:55:37 +0530

    admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,8 @@ router.get('/adminLogout', adminController.adminLogout)
 router.get('/user-management', adminController.userManage);
 router.get('/blockUser/:id', adminController.blockUser);
 router.get('/unblockUser/:id', adminController.unblockUser);
+
+//banner_router
 router.get('/bannerManagement', sessionChecker.adminAuth, adminController.bannerManagement);
 router.get('/add-banner', adminController.addBannerGet);
 router.post('/add-banner', upload.single("image"), adminController.addBannerPost);
@@ -20,11 +22,15 @@ router.get('/unlist-banner/:id', adminController.unlistBanner);
 router.get('/edit-banner/:id', adminController.editBanner);
 router.post('/edit-banner/:id', adminController.editBannerPost);
 router.get('/deleteBanner/:id', adminController.deleteBanner);
+
+//cateogory_router
 router.post('/addCategory', adminController.addCategoryPost);
 router.get('/category', sessionChecker.adminAuth, adminController.categoryManagement);
 router.get('/listcategory/:id', adminController.listcategory);
 router.get('/unlistcategory/:id', adminController.unlistcategory);
 router.post('/editCategory/:id', adminController.editCategoryPost);
+
+//products_router
 router.get('/view-products', sessionChecker.adminAuth, adminController.viewProducts);
 router.get('/add-product', sessionChecker.adminAuth, adminController.addProductGet);
 router.post('/add-product', upload.array("image"), adminController.addProductPost);
@@ -32,19 +38,25 @@ router.get('/editProduct/:id', adminController.editProduct);
 router.post('/editProduct/:id', upload.array("image"), adminController.editProductPost);
 router.get('/listProduct/:id', adminController.listProduct);
 router.get('/unlistProduct/:id', adminController.unlistProduct);
+
+//coupon_router
 router.get('/coupon', sessionChecker.adminAuth, adminController.getCoupons);
 router.post('/addCoupon', adminController.addCouponPost);
 router.get('/activateCoupon/:id', adminController.activateCoupon);
 router.get('/deactivateCoupon/:id', adminController.deactivateCoupon);
 router.post('/editCoupon/:id', adminController.editCouponPost);
 router.get('/deleteCoupon/:id', adminController.deleteCoupon);
+
+//oredr_router
 router.get('/orderManagement', sessionChecker.adminAuth, adminController.orderManagement);
 router.get('/singleOrderDetail/:id', sessionChecker.adminAuth, adminController.singleOrderDetail);
 router.get('/cancelOrder/:id', adminController.cancelOrder);
 router.get('/shipOrder/:id', adminController.shipOrder);
 router.get('/orderDelivered/:id', adminController.orderDelivered);
 
+//chart
 router.get('/chart-details', adminController.chartDetails);
 
+//sales
 router.get('/salesReport', adminController.salesReport);
 module.exports = router;
